5adf7431937d3ed669d65b4f7e78f49bdcdb3e7c
aj8067356 <132342215+aj8067356@users.noreply.github.com>
2023-05-02 15:53:00 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+//array
+let numbers=[1,2,3,4,5,6,7,8,9,10];
+console.log(numbers);
+printArray=(array)=>{
+    for(let i=0;i<=9;i++){
+        console.log(array[i]);
+    }
+}
+printArray(numbers);
